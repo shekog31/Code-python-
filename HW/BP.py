838c5fad2d314392a4eb563a28a027ab2852c812
shekog31 <shekhar.tampa@gmial.com>
2024-11-28 18:53:41 -0500

    BP2

---
+++ HW/BP.py
@@ -0,0 +1,11 @@
+a= "2000-01-15"
+b="2000-01-14"
+c="2000-01-13"
+d="2000-01-12"
+e="2000-01-11"
+
+print("Friend 1's birthday:",a)
+print("Friend 2's birthday:",b)
+print("Friend 3's birthday:",c)
+print("Friend 4's birthday:",d)
+print("Friend 5's birthday:",e)
