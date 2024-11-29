118cb988ed33d35c6c0a638f95c8859d00a2892a
shekog31 <shekhar.tampa@gmial.com>
2024-11-28 19:11:32 -0500

    age

---
+++ HW/age.py
@@ -0,0 +1,8 @@
+a=int(input("How old are you?:"))
+if a>10:
+    if a>=10 and a<=20:
+        print("You are in the ages between 10 and 20")
+    else:
+        print("Not allowed")
+else:
+    print("Not elgible to be here")
---
+++ HW/weath.py
@@ -0,0 +1,1 @@
+print(input("What is the temperature?: "))
