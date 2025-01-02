ac3c2dfe482414da045c6752491786cb5c44643c
shekog31 <shekhar.tampa@gmial.com>
2025-01-02 18:28:29 -0500

    AL

---
+++ HW/AL.py
@@ -0,0 +1,56 @@
+le= input("Enter character: ")
+
+if le=="a" or "A":
+    print("It is a letter of the alphabet")
+elif le=="b" or "B":
+    print("It is a letter of the alphabet")
+elif le=="c" or "C":
+    print("It is a letter of the alphabet")
+elif le=="d" or "D":
+    print("It is a letter of the alphabet")
+elif le=="e" or "E":
+    print("It is a letter of the alphabet")
+elif le=="f" or "F":
+    print("It is a letter of the alphabet")
+elif le=="g" or "G":
+    print("It is a letter of the alphabet")
+elif le=="h" or "H":
+    print("It is a letter of the alphabet")
+elif le=="i" or "I":
+    print("It is a letter of the alphabet")
+elif le=="j" or "J":
+    print("It is a letter of the alphabet")
+elif le=="k" or "K":
+    print("It is a letter of the alphabet")
+elif le=="l" or "L":
+    print("It is a letter of the alphabet")
+elif le=="m" or "M":
+    print("It is a letter of the alphabet")
+elif le=="n" or "N":
+    print("It is a letter of the alphabet")
+elif le=="o" or "O":
+    print("It is a letter of the alphabet")
+elif le=="p" or "P":
+    print("It is a letter of the alphabet")
+elif le=="q" or "Q":
+    print("It is a letter of the alphabet")
+elif le=="r" or "R":
+    print("It is a letter of the alphabet")
+elif le=="s" or "S":
+    print("It is a letter of the alphabet")
+elif le=="t" or "T":
+    print("It is a letter of the alphabet")
+elif le=="u" or "U":
+    print("It is a letter of the alphabet")
+elif le=="v" or "V":
+    print("It is a letter of the alphabet")
+elif le=="w" or "W":
+    print("It is a letter of the alphabet")
+elif le=="x" or "X":
+    print("It is a letter of the alphabet")
+elif le=="y" or "Y":
+    print("It is a letter of the alphabet")
+elif le=="z" or "Z":
+    print("It is a letter of the alphabet")
+else:
+    print("It is not a letter of the alphabet")
