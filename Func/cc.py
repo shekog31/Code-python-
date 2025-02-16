3e151988f74bd3413c8ec33e423d1878d214cf1a
shekog31 <shekhar.tampa@gmial.com>
2025-02-16 11:34:21 -0500

    cc

---
+++ Func/cc.py
@@ -0,0 +1,38 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def mod(a,b):
+    return a%b
+
+print("{lease pick operation")
+print("a.add")
+print("b.sub")
+print("c.mul")
+print("d.div")
+print("e.mod")
+choice=input("Please pick the choice(a/b/c/d/e)")
+
+num1=int(input("enter value 1:"))
+num2=int(input("enter value 2:"))
+
+if choice=='a':
+    print(num1,"+" ,num2,"=" ,add(num1,num2))
+elif choice=='b':
+    print(num1,"-" ,num2,"=" ,sub(num1,num2))
+elif choice=='c':
+    print(num1,"x" ,num2,"=" ,mul(num1,num2))
+elif choice=='d':
+    print(num1,"/" ,num2,"=" ,div(num1,num2))
+elif choice=='e':
+    print(num1,"%" ,num2,"=" ,mod(num1,num2))
+else:
+    print("Input invalid")
