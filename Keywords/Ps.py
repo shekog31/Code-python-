c34d9b048cf779a021f0dd797458efcf2b678e6d
shekog31 <shekhar.tampa@gmial.com>
2025-03-06 18:54:04 -0500

    Ps

---
+++ Keywords/Ps.py
@@ -0,0 +1,11 @@
+for x in range(10):
+    if x%20==0:
+        print("clock")
+    elif x%15==0:
+        pass
+    elif x%5==0:
+        print("lip")
+    elif x%3==0:
+        print("trop")
+    else:
+        print(x)
