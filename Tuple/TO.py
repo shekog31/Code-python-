f1d70672184baeca86d12d2f491925a0020c6e1a
shekog31 <shekhar.tampa@gmial.com>
2025-05-11 12:19:07 -0400

    TO

---
+++ Tuple/TO.py
@@ -0,0 +1,18 @@
+tuplex=("tuple", False, 3.3, 2)
+print(tuplex)
+
+tuplex=(2, 4, 6, 8, 10, 12)
+print(tuplex)
+tuplex=tuplex + (14,)
+print(tuplex)
+
+tuple1=(10, 20, 30, 30, 20, 10)
+print(tuple1.count(10))
+
+tuplex=(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+slice=tuplex[3:6]
+
+print(slice)
+
+slice=tuplex[:4]
+print(slice)
