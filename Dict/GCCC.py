2f212cea394cdef01b0dec55e9181b39d99ef1ed
shekog31 <shekhar.tampa@gmial.com>
2025-05-31 19:33:34 -0400

    GCCC

---
+++ Dict/GCCC.py
@@ -0,0 +1,9 @@
+SPCMc_c={'India': '0091',
+         'Australia:': '0025',
+         'Nepal': '00977'}
+
+print("Country code for india-")
+print(SPCMc_c.get('India', 'Not Found'))
+
+print("Country code for Japan-")
+print(SPCMc_c.get('Japan', 'Not found'))
