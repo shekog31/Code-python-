3808c2d2123aa209d385812eb69748b7568de6e3
shekog31 <shekhar.tampa@gmial.com>
2025-06-28 19:18:07 -0400

    ONS

---
+++ Sets and arrays/ONS.py
@@ -0,0 +1,18 @@
+ms={1,4,3}
+print(ms)
+
+mst={3.0, "Hi", (1,4,3)}
+print(mst)
+
+msb={1,1,1,4,4,3,3}
+print(msb)
+
+msc=set([1,4,3,4])
+print(msc,"\n")
+
+ns=set([0,1,4,3,6])
+print("OG set:")
+print(ns)
+ns.pop()
+print("After removing the first element from the set:")
+print(ns, "\n")
