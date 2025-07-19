56bdc6c3990d8513205989c2e00864e44813f7cf
shekog31 <shekhar.tampa@gmial.com>
2025-07-19 19:23:57 -0400

    cvv

---
+++ Class/cvv.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+mdX = Vehicle(240, 18)
+
+print("Model Max Speed:",mdX.max_speed) 
+print("Model Mileage",mdX.mileage)  
+    
